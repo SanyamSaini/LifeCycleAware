7a175685e13c39e955c0d857a95e81655d57f52e
Sanyam Saini <sanyamsaini51@gmail.com>
2023-02-06 16:57:48 +0530

    Tried tying an observer class with the main activity and added logs for updates over the callbacks.

--- app/src/main/java/com/life/cycle/aware/MainActivity.kt
+++ app/src/main/java/com/life/cycle/aware/MainActivity.kt
@@ -2,10 +2,33 @@ package com.life.cycle.aware
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        lifecycle.addObserver(Observer())
+        Log.d("MAIN", "ACTIVITY - ON CREATE")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MAIN", "ACTIVITY - ON PAUSE")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MAIN", "ACTIVITY - ON RESUME")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MAIN", "ACTIVITY - ON STOP")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MAIN", "ACTIVITY - ON DESTROY")
     }
 }
---
+++ app/src/main/java/com/life/cycle/aware/Observer.kt
@@ -0,0 +1,35 @@
+package com.life.cycle.aware
+
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+class Observer : LifecycleObserver {
+
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreate() {
+        Log.d("MAIN", "OBSERVER - ON CREATE")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onResume() {
+        Log.d("MAIN", "OBSERVER - ON RESUME")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onPause() {
+        Log.d("MAIN", "OBSERVER - ON PAUSE")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onStop() {
+        Log.d("MAIN", "OBSERVER - ON STOP")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroy() {
+        Log.d("MAIN", "OBSERVER - ON DESTROY")
+    }
+}
